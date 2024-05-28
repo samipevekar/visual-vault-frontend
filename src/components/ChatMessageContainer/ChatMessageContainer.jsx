d16395355639f3ebfcafb1e9dbaa934ce5171e95
sami pevekar <sami@p.com>
2024-05-28 13:48:18 +0530

    updated caht component

--- src/components/ChatMessageContainer/ChatMessageContainer.jsx
+++ src/components/ChatMessageContainer/ChatMessageContainer.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from 'react-icons/ti'
 import { FaArrowLeft } from "react-icons/fa";
 import useConversation from '../../zustand/userConversation'
 import './ChatMessageContainer.css'
+import shopContext from '../../Context/ShopContext';
 
 export default function ChatMessageContainer() {
 
   const {selectedConversation,setSelectedConversation} = useConversation();
+  const {onlineUsers} = useContext(shopContext)
+  const isOnline = onlineUsers.includes(selectedConversation?._id)
   
   return (
-    <div className={`min-w-[400px] max-w-[650px] bg-slate-50 messageContainer ${selectedConversation ? 'z-10' : ''}`}>
+    <div className={`min-w-[400px] max-w-[650px]  messageContainer ${selectedConversation ? 'z-10' : ''}`}>
        {!selectedConversation ? (<NoChatSelected/>) :
         (<><div className=' px-4 py-2 mb-2 flex items-center border-b-2  ' >
                 <FaArrowLeft className='cursor-pointer text-black' onClick={()=>setSelectedConversation(null)} />
-                <img src={selectedConversation.profilePic} className='w-8 h-8 ml-4 border-black border-1 rounded-full' alt="" />
+            <div className={`ml-4  avatar w-8 h-8 ${isOnline ? 'online':''} `}>
+            <div className="w-12 rounded-full">
+                <img src={selectedConversation.profilePic}  onError={(e) => {
+                e.target.src = user1;
+            }} />
+            </div>
+        </div>
                 <span className='text-black font-bold mx-2 '>{selectedConversation.name}</span>{" "}
+        {/* <div>
+         {isOnline && <span className='text-[10px]'>Active now</span>}
+        </div> */}
         </div>
         <Messages/>
         <MessageInput/></>)
