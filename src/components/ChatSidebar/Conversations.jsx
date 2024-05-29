e04f0d6eb4405eb14bce4ba45f195e8ebb62e746
samipevekar <samipevekar499@gmail.com>
2024-05-29 12:17:11 +0530

    sorted online users for sidebar

--- src/components/ChatSidebar/Conversations.jsx
+++ src/components/ChatSidebar/Conversations.jsx
@@ -1,22 +1,37 @@
-import React, { useContext, useEffect } from 'react'
-import Conversation from './Conversation'
-import shopContext from '../../Context/ShopContext'
-import ConversationSkeleton from '../skeletons/ConversationSkeleton'
+import React, { useContext, useEffect } from 'react';
+import Conversation from './Conversation';
+import shopContext from '../../Context/ShopContext';
+import ConversationSkeleton from '../skeletons/ConversationSkeleton';
 
 export default function Conversations() {
-  const {allUsers,getAllUsers,loading} = useContext(shopContext)
+  const { allUsers, getAllUsers, loading, onlineUsers } = useContext(shopContext);
 
-  useEffect(()=>{
-    getAllUsers()
-  },[])
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  // Function to sort users based on online status
+  const sortedUsers = allUsers.slice().sort((a, b) => {
+    const aIsOnline = onlineUsers.includes(a._id);
+    const bIsOnline = onlineUsers.includes(b._id);
+    if (aIsOnline && !bIsOnline) return -1;
+    if (!aIsOnline && bIsOnline) return 1;
+    return 0;
+  });
 
   return (
-    <div className='overflow-auto flex flex-col ' >
-      {allUsers && allUsers.map((conversation,idx)=>{
-        return <Conversation key={conversation._id} conversation={conversation} name={conversation.name} profilePic={conversation.profilePic} lastIdx={idx === allUsers.length - 1} />
-      })}
+    <div className='overflow-auto flex flex-col'>
+      {sortedUsers && sortedUsers.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          name={conversation.name}
+          profilePic={conversation.profilePic}
+          lastIdx={idx === sortedUsers.length - 1}
+        />
+      ))}
 
-      {loading && [...Array(5)].map((_,idx)=> <ConversationSkeleton key={idx} />) }
+      {loading && [...Array(5)].map((_, idx) => <ConversationSkeleton key={idx} />)}
     </div>
-  )
+  );
 }
