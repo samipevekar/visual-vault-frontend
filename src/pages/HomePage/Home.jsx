4d738593ce8e7025159a808019e830e5c4b3ad6f
samipevekar <samipevekar499@gmail.com>
2024-07-04 21:56:23 +0530

    DisplayImage component changed to modal

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Home.css';
 import { Routes, Route,Navigate } from 'react-router-dom';
-import Aside from "../../components/Aside/Aside"
 import Loader from '../../components/Loader/Loader';
-import About from '../../components/About/About';
-import HomeContent from '../../components/HomeContent/HomeContent';
+const  Aside = React.lazy(()=>import("../../components/Aside/Aside")) ;
+const About = React.lazy(()=>import('../../components/About/About'))  ;
+const HomeContent = React.lazy(()=>import('../../components/HomeContent/HomeContent')) ;
 const Addimage =  React.lazy(()=>import('../../components/Addimage/Addimage')) ;
 const Collection = React.lazy(()=>import('../../components/Collection/Collection')) ;
 const Favorites = React.lazy(()=>import('../../components/Favorites/Favorites')) ;
@@ -19,16 +19,16 @@ export default function Home() {
 
   return (
     <div className='homepage'>
-      <Aside />
+      <React.Suspense fallback={<Loader/>}><Aside /></React.Suspense>
       <div className='homeComponents'>
+          <DisplayImage/> 
         <Routes>
-          <Route path="/" element={<HomeContent />} />
+          <Route path="/" element={<React.Suspense fallback={<Loader/>}><HomeContent /></React.Suspense>} />
           <Route path="/addimage" element={authUser ? <React.Suspense fallback={<Loader/>}><Addimage /></React.Suspense>  : <Navigate to="/signup"/>} />
           <Route path="/collections" element={authUser ? <React.Suspense fallback={<Loader/>}><Collection /></React.Suspense>: <Navigate to="/signup"/>}/>
           <Route path="/favorites" element={authUser ? <React.Suspense fallback={<Loader/>}><Favorites /></React.Suspense> : <Navigate to="/signup"/>} />
           <Route path="/displaysearch" element={authUser ? <React.Suspense fallback={<Loader/>}><DisplaySearch /></React.Suspense> : <Navigate to="/signup"/>} />
-          <Route path="/about" element={<About />} />
-          <Route path="/image/:imageId" element={authUser ? <React.Suspense fallback={<Loader/>}><DisplayImage /></React.Suspense> : <Navigate to="/signup"/>} />
+          <Route path="/about" element={<React.Suspense fallback={<Loader/>}><About /></React.Suspense>} />
           <Route path="/chats" element={authUser ? <React.Suspense fallback={<Loader/>}><Chats /></React.Suspense> : <Navigate to="/signup"/>} />
           <Route path="/signup" element={<React.Suspense fallback={<Loader/>}><Signup /></React.Suspense>}></Route>
           <Route path="*" element={<HomeContent/>}></Route>
